c0844100c54954eaba594fa9cedddc621869d130
Leandro Cabeda Rigo <leandro.cabeda@hotmail.com>
2025-03-03 00:16:14 -0300

    configuration: adicionado configuração do banco postgresql, automapper, mediatR e serviço do consumidor de UserService

--- template/backend/src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/ApplicationModuleInitializer.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/ApplicationModuleInitializer.cs
@@ -1,6 +1,9 @@
-﻿using Ambev.DeveloperEvaluation.Common.Security;
+﻿using Ambev.DeveloperEvaluation.Application.Services.Implementations;
+using Ambev.DeveloperEvaluation.Application.Services.Interfaces;
+using Ambev.DeveloperEvaluation.Common.Security;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Ambev.DeveloperEvaluation.IoC.ModuleInitializers;
 
@@ -9,5 +12,12 @@ public class ApplicationModuleInitializer : IModuleInitializer
     public void Initialize(WebApplicationBuilder builder)
     {
         builder.Services.AddSingleton<IPasswordHasher, BCryptPasswordHasher>();
+        // Adiciona o mapeamento automático
+        builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        // Adiciona o MediatR
+        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        // Adiciona o serviço
+        builder.Services.AddScoped<IUserService, UserService>();
+
     }
 }
